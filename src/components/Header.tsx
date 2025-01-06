1690ae118b5cbe092fc7f24e8e23c2515bcfad5e
Khoai <khoa0310st@gmail.com>
2025-01-06 17:01:56 +0700

    update animation for header and update lib  Framer Motion

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import {
@@ -6,13 +8,19 @@ import {
   FaInstagram,
   FaPhoneAlt,
   FaTelegram,
-  FaTiktok,
 } from "react-icons/fa";
+
+import { SiZalo } from "react-icons/si";
 
 export default function Header() {
   return (
     <>
-      <div className="bg-pink-700 text-white text-sm">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.5 }}
+        className="bg-pink-700 text-white text-sm"
+      >
         <div className="container mx-auto flex justify-between items-center py-2 px-4">
           <div className="flex items-center space-x-4">
             <a
@@ -40,12 +48,12 @@ export default function Header() {
               <FaFacebook />
             </a>
             <a
-              href="https://tiktok.com"
+              href="https://zalo.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="hover:text-gray-300"
+              className="hover:text-gray-300 "
             >
-              <FaTiktok />
+              <SiZalo />
             </a>
             <a
               href="https://instagram.com"
@@ -65,8 +73,13 @@ export default function Header() {
             </a>
           </div>
         </div>
-      </div>
-      <header className="text-gray-300 body-font bg-white sticky top-0 z-50 shadow-md">
+      </motion.div>
+      <motion.header
+        className="text-gray-300 body-font bg-white sticky top-0 z-50 shadow-md"
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+      >
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto font-dosis font-extrabold">
             <Link
@@ -98,7 +111,10 @@ export default function Header() {
               <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-pink-500 transition-all duration-300 group-hover:w-full"></span>
             </Link>
           </nav>
-          <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0 hover:text-pink-500 transition duration-300 transform hover:scale-110">
+          <motion.a
+            whileHover={{ scale: 1.1, rotate: 5 }}
+            className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0 hover:text-pink-500 transition duration-300 transform hover:scale-110"
+          >
             <Image
               src="/images/logo.jpg"
               alt="POMY PETSHOP Logo"
@@ -106,7 +122,7 @@ export default function Header() {
               width={85}
               height={85}
             />
-          </a>
+          </motion.a>
 
           <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0 mt-5 lg:mt-0">
             <button className="animate-bounce inline-flex items-center bg-pink-500 text-white font-semibold py-2 px-6 rounded-full shadow-lg transition-all duration-300 ease-in-out transform group">
@@ -117,7 +133,7 @@ export default function Header() {
             </button>
           </div>
         </div>
-      </header>
+      </motion.header>
     </>
   );
 }

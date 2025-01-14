ec33d788bc03c1f8d2bcf9698a82d4b7efd2962d
Khoai <khoa0310st@gmail.com>
2025-01-14 17:20:08 +0700

    update css

--- src/components/QuestionSection.tsx
+++ src/components/QuestionSection.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 function QuestionSection() {
   return (
     <section className="text-gray-600 body-font">
-      <div className="container mx-auto flex px-5 pb-24 pt-8 md:flex-row flex-col items-center">
+      <div className="container mx-auto flex px-5 py-8 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <motion.span
             className="title-font sm:text-2xl text-2xl mb-1 text-green-500 font-extrabold"
@@ -38,7 +38,36 @@ function QuestionSection() {
             bạn.
           </motion.span>
 
-          <div className="flex flex-col mt-6 mb-10 lg:items-start items-center">
+          <div className="flex flex-col md:flex-row md:gap-3  mt-6 mb-10 lg:items-start items-center">
+            <div>TEXT</div>
+            <motion.div
+              className="flex-grow"
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6, delay: 0.4 }}
+              viewport={{ once: true, amount: 0.5 }}
+            >
+              <h2 className="text-pink-600 text-lg title-font font-semibold mb-3">
+                Chăm sóc tận tâm và chu đáo
+              </h2>
+              <p className="leading-relaxed text-base">
+                <span className="text-pink-600 font-dosis font-bold">
+                  <a
+                    href="https://www.facebook.com/cuahangthucungPOMY"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline"
+                  >
+                    Pomy Petshop{" "}
+                  </a>
+                </span>
+                luôn đặt sức khỏe và hạnh phúc của thú cưng lên hàng đầu, mang
+                đến sự yên tâm và tin tưởng tuyệt đối cho bạn.
+              </p>
+            </motion.div>
+          </div>
+
+          <div className="flex flex-col mb-10 lg:items-start items-center">
             <motion.div
               className="flex-grow"
               initial={{ opacity: 0, y: 20 }}
@@ -70,34 +99,6 @@ function QuestionSection() {
               <p className="leading-relaxed text-base">
                 Phòng khám được trang bị các thiết bị y tế tiên tiến, đảm bảo
                 chẩn đoán chính xác và điều trị hiệu quả.
-              </p>
-            </motion.div>
-          </div>
-
-          <div className="flex flex-col mb-10 lg:items-start items-center">
-            <motion.div
-              className="flex-grow"
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.6, delay: 0.4 }}
-              viewport={{ once: true, amount: 0.5 }}
-            >
-              <h2 className="text-pink-600 text-lg title-font font-semibold mb-3">
-                Chăm sóc tận tâm và chu đáo
-              </h2>
-              <p className="leading-relaxed text-base">
-                <span className="text-pink-600 font-dosis font-bold">
-                  <a
-                    href="https://www.facebook.com/cuahangthucungPOMY"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="hover:underline"
-                  >
-                    Pomy Petshop{" "}
-                  </a>
-                </span>
-                luôn đặt sức khỏe và hạnh phúc của thú cưng lên hàng đầu, mang
-                đến sự yên tâm và tin tưởng tuyệt đối cho bạn.
               </p>
             </motion.div>
           </div>
